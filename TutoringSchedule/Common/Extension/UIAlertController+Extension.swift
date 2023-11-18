eb74fa275b5b7e447ef921e16d5e59bbc52f1f9a
ha-ny <hany01.dev@gmail.com>
2023-11-19 02:45:27 +0900

    전반적인 코드 수정

--- TutoringSchedule/Common/Extension/UIAlertController+Extension.swift
+++ TutoringSchedule/Common/Extension/UIAlertController+Extension.swift
@@ -8,9 +8,13 @@
 import UIKit
 
 extension UIAlertController {
-    static func customMessageAlert(view: UIViewController, title: String, message: String) {
+    static func customMessageAlert(view: UIViewController, title: String, message: String, _ completion: (() -> ())? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let ok = UIAlertAction(title: "alertOKButtonTitle".localized, style: .default)
+        
+        let ok = UIAlertAction(title: "alertOKButtonTitle".localized, style: .default) { _ in
+            completion?()
+        }
+        
         alert.addAction(ok)
         view.present(alert, animated: true)
     }
