a77f3fb953e8eb9cd3adb030668bc516942c5361
ha-ny <hany01.dev@gmail.com>
2023-11-19 02:41:25 +0900

    customToast 생성

---
+++ TutoringSchedule/Common/CustomToast.swift
@@ -0,0 +1,19 @@
+//
+//  customToast.swift
+//  TutoringSchedule
+//
+//  Created by 김하은 on 11/19/23.
+//
+
+import Foundation
+import Toast
+
+class CustomToast {
+    static func setting() -> ToastStyle {
+        var style = ToastStyle()
+        style.backgroundColor = .systemGray6
+        style.messageColor = .signatureColor
+        style.messageFont = .boldSystemFont(ofSize: 13)
+        return style
+    }
+}
